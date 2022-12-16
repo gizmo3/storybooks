f97ebc184e4b0a0a08b2540e2c8270eb0c2e6e03
Jordan James <jordanj1303@yahoo.com.au>
2022-12-17 09:47:02 +1100

    finally got it working

--- app.js
+++ app.js
@@ -8,4 +8,7 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT, 
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
